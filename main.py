4f879a1ea6da554b85a37109f16c76432a50199f
calin <chelceacalin1@gmail.com>
2026-02-20 21:19:09 +0200

    project structure

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import os
+load_dotenv()
+tools =[]
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=os.getenv("GOOGLE_API_KEY"),
+    temperature=0,
+).bind_tools(tools)
+
 def main():
-    print("Hello from bookingagent!")
+    print(llm.invoke("hi"))
 
 
 if __name__ == "__main__":
